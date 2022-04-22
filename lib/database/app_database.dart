629f4a3699c825dacd4b943310d0d514e361dbd9
LeoCosta <leocosta.black@hotmail.com>
2022-04-22 17:55:10 -0300

    Refactor to use 'async/await' in database file

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,60 +2,48 @@ import 'package:masterbank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
+Future<Database> getDatabase() async {
+  // OBS: O caminho é criando automaticamente pelos pacotes, você só precisa definir i nome do arquivo que irá guardar seu banco de dados... Sempre use a extenção ".db"
+  final String path = join(await getDatabasesPath(), 'masterbank.db');
   // Retorna o banco de dados para poder ser usado na aplicação
-  return getDatabasesPath().then((dbPath) {
-    // Definindo o caminho do arquivo para a Database local.
-    // OBS: O caminho é criando automaticamente pelos pacotes, você só precisa definir i nome do arquivo que irá guardar seu banco de dados... Sempre use a extenção ".db"
-    final String path = join(dbPath, 'masterbank.db');
-    return openDatabase(
-      path,
-      onCreate: (db, version) => {
-        // Criando uma tabela no banco de dados local
-        db.execute('CREATE TABLE contacts('
-            'id INTEGER PRIMARY KEY, '
-            'account_name TEXT, '
-            'account_number INTEGER)')
-      },
-      // Definindo a versão do banco de dados local
-      /// Para limpar a database atual basta seguir os seguintes passos:
-      /// Incluir a propriedade "onDowngrade" > Colocar uma nova versão > Executar o aplicativo > Reduzir o numero da versão > Executar o aplicativo novamente
-      version: 2,
-      onDowngrade: onDatabaseDowngradeDelete,
+  return openDatabase(
+    path,
+    onCreate: (db, version) => {
+      // Criando uma tabela no banco de dados local
+      db.execute('CREATE TABLE contacts('
+          'id INTEGER PRIMARY KEY, '
+          'account_name TEXT, '
+          'account_number INTEGER)')
+    },
+    // Definindo a versão do banco de dados local
+    version: 1,
+  );
+}
+
+Future<int> save(Contact contact) async {
+  final Database db = await getDatabase();
+  // Mapa para definir os valores que seram guardados no banco de dados local
+  final Map<String, dynamic> contactMap = {};
+  // O campo "id" foi omitido pois o sqlite irá gerar números inteiros random para ele já que é a chave primária
+  contactMap['account_name'] = contact.accountName;
+  contactMap['account_number'] = contact.accountNumber;
+  // Inserir valores na tabela "contacts"
+  return db.insert('contacts', contactMap);
+}
+
+Future<List<Contact>> findAll() async {
+  final Database db = await getDatabase();
+  // Busca todos os dados da tabela "contacts"
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+  final List<Contact> contacts = [];
+  for (Map<String, dynamic> resultValue in result) {
+    final Contact contact = Contact(
+      resultValue['id'],
+      resultValue['account_name'],
+      resultValue['account_number'],
     );
-  });
-}
+    contacts.add(contact);
+  }
 
-Future<int> save(Contact contact) {
-  // Retorna o ID
-  return createDatabase().then((db) {
-    // Mapa para definir os valores que seram guardados no banco de dados local
-    final Map<String, dynamic> contactMap = {};
-    // O campo "id" foi omitido pois o sqlite irá gerar números inteiros random para ele já que é a chave primária
-    contactMap['account_name'] = contact.accountName;
-    contactMap['account_number'] = contact.accountNumber;
-    // Inserir valores na tabela "contacts"
-    return db.insert('contacts', contactMap);
-  });
-}
-
-Future<List<Contact>> findAll() {
-  // Retorna a lista de contatos
-  return createDatabase().then((db) {
-    // Busca todos os dados da tabela "contacts"
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = [];
-
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact = Contact(
-          map['id'],
-          map['account_name'],
-          map['account_number'],
-        );
-        contacts.add(contact);
-      }
-
-      return contacts;
-    });
-  });
+  return contacts;
 }
